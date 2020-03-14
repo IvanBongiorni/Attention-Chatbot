232e3fd4d0c449f8af1ad2a2ad1648d57879dda5
ivan <ivanbongiorni@gmail.com>
2020-03-14 23:53:49 +0100

    fixed function

--- ml/train.py
+++ ml/train.py
@@ -1,8 +1,19 @@
+"""
+Author: Ivan Bongiorni,     https://github.com/IvanBongiorni
+2020-03-14
+
+TRAINING FUNCTION
+
+Contains a single training function that is meant to be neutral, and that can be called for any chatbot implementation.
+It's an Autograph function: the @tf.function decorator tranforms train_on_batch() inner function into its TensorFlow graph representation.
+"""
+
 
 def train_model(model, params, X_train, Y_train, X_val, Y_val):
     '''
     Training Autograph function
     '''
+    import time
     import numpy as np
     import tensorflow as tf
     
@@ -20,16 +31,16 @@ def train_model(model, params, X_train, Y_train, X_val, Y_val):
         optimizer.apply_gradients(zip(gradients, model.trainable_variables))
         return current_loss
     
-    optimizer = tf.keras.optimizers.Adam(learning_rate = learning_rate)
+    optimizer = tf.keras.optimizers.Adam(learning_rate = params['learning_rate'])
     
-    loss_history = []
+    # loss_history = []
     
-    for epoch in range(n_epochs):
+    for epoch in range(params['n_epochs']):
         start = time.time()
         
-        for iteration in range(X_train.shape[0] // batch_size):
+        for iteration in range(X_train.shape[0] // params['batch_size']):
             current_loss = train_on_batch()
-            loss_history.append(current_loss)
+            # loss_history.append(current_loss)
     
         validation_loss = loss(Y_val, model(X_val))
         
@@ -44,4 +55,3 @@ def train_model(model, params, X_train, Y_train, X_val, Y_val):
     
     print('Model saved at:\n\t{}'.format(params['save_path'])
     return None
-
