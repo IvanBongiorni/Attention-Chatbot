5532bafc7d8cabed15172aee28c5efd42f43c3db
ivan <ivanbongiorni@gmail.com>
2020-03-16 23:43:49 +0100

    model sketch improved-shapes tackled

--- ml/model.py
+++ ml/model.py
@@ -4,109 +4,65 @@ Author: Ivan Bongiorni,     https://github.com/IvanBongiorni
 
 MODEL BUILDING
 
+This is a seq2seq model with Convolutional pseudo-Attention. It differs from canonical Attention since 
+its scores are not calculated iteratively, as the output of the model progresses. All scores are computed
+at once, stacked with the Encoder's output, and fed into the Decoder LSTM layer.
 
-
-build_model() is a wrapper, instantiating and assembling all the parts.
+With it I expect to be able to put together two benefits of RNNs and CNNs. RNNs are perfect in order to 
+process time dependent data, but their problem is dealing with long-term dependency: all Encoder output 
+information has to be squeezed into a single vector. 1D Conv layers are meant intestead to process time series 
+in a different way, and to "bring time back" to the Decoder.
 """
-
-
-params['vocab_size']
-params['encoder_units']
-params['decoder_units']
-
 
 
 def build(params):
     import tensorflow as tf
     from tensorflow.keras.models import Model
-    from tensorflow.keras.layers import Input, Embedding, LSTM, Dense
+    from tensorflow.keras.layers import Input, Embedding, LSTM, RepeatVector, Conv1D, Dense, TimeDistributed
+    
+
+    # ENCODER
+    encoder_input = Input()
+    
+    encoder_embedding = Embedding(params['vocab_size'], params['embedding_size'])(encoder_input)
+    
+    encoder_output = LSTM(params['encoder_lstm_units'], 
+                          return_sequences = True, 
+                          recurrent_initializer = params['recurrent_initializer'])(encoder_embedding)
+    
+    encoder_output = RepeatVector(params['decoder_units'])
+    
+    
+    # Convolutional Pseudo-Attention
+    conv_1 = Conv1D(filters = params['conv_filters'][0], 
+                    kernel_size = 3, padding = 'same', activation = params['conv_activation'])
+    conv_2 = Conv1D(filters = params['conv_filters'][1], 
+                    kernel_size = 3, padding = 'same', activation = params['conv_activation'])
+    conv_3 = Conv1D(filters = params['conv_filters'][0], 
+                    kernel_size = 3, padding = 'same', activation = params['conv_activation'])
+    # scores = Dense(params['decoder_units'], activation = 'softmax')(conv)
+    
+    
+    # Concatenation of Encoder and Pseudo-Attention scores
+    concatenation = Concatenate([encoder_output, scores], axis = )
+    
+    
+    # DECODER
+    decoder_output = LSTM(params['decoder_lstm_units'],
+                          return_sequences = True,
+                          recurrent_initializer = params['recurrent_initializer'])(concatenation)
+    
+    decoder_dense_1 = TimeDistributed(Dense(params['decoder_dense_units'], 
+                                            activation = params['decoder_dense_activation']))(decoder_output)
+    decoder_dense_2 = TimeDistributed(Dense(params['decoder_dense_units'], 
+                                            activation = params['decoder_dense_activation']))(decoder_dense_1)
+    decoder_output = TimeDistributed(Dense(1, activation = None))(decoder_dense_2)
+    
+    
+    model = Model(inputs = [encoder_input], outputs = [decoder_output])
     
     return model
 
 
-
-
-# ENCODER
-encoder_input = Input()
-encoder_embedding = Embedding(params['vocab_size'], params['embedding_size'])(encoder_input)
-encoder_output, encoder_state = LSTM(params['encoder_units'], 
-                                     return_sequences = True, 
-                                     return_state = True, 
-                                     recurrent_initializer = params['recurrent_initializer'])(encoder_embedding)
-
-
-# ATTENTION
-query_with_time_axis = tf.expand_dims(encoder_state, 1)
-
-W1 = Dense(params['decoder_units'])(query_with_time_axis)
-W2 = Dense(params['decoder_units'])(encoder_output)
-score = tf.nn.tanh(W1 + W2)
-score = Dense(1)(score)
-
-attention_weights = tf.nn.softmax(score, axis = 1)
-
-context_vector = attention_weights * values
-context_vector = tf.reduce_sum(context_vector, axis = 1)
-
-
-# DECODER
-
-# CREDO che la x qui sia encoder_output, ma devo assicurarmene
-decoder_input = Input()
-
-x = Embedding(params['vocab_size'], params['embedding_size'])(x)
-x = tf.concat([tf.expand_dims(context_vector, 1), x], axis = -1)
-
-decoder_output, decoder_state = LSTM(params['decoder_units'], 
-                                     return_sequences = True, 
-                                     return_state = True, 
-                                     recurrent_initializer = params['recurrent_initializer'])(x)
-
-decoder_output = tf.reshape(decoder_output, (-1, decoder_output.shape[2]))
-
-decoder_output = Dense(params['vocab_size'])(decoder_output)
-
-
-
-
-
-
-
-# decoder(dec_input, dec_hidden, enc_output)
-
-
-
-
-@tf.function
-def train_step(inp, targ, enc_hidden):
-    loss = 0
-    
-    with tf.GradientTape() as tape:
-        
-        enc_output, enc_hidden = encoder(inp)
-        
-        dec_hidden = enc_hidden
-        
-        dec_input = tf.expand_dims([targ_lang.word_index['<start>']] * params['batch_size'], 1)
-        
-        # Teacher forcing - feeding the target as the next input
-        for t in range(1, targ.shape[1]):
-            # passing enc_output to the decoder
-            predictions, dec_hidden, _ = decoder(dec_input, dec_hidden, enc_output)
-
-            loss += loss_function(targ[:, t], predictions)
-
-            # using teacher forcing
-            dec_input = tf.expand_dims(targ[:, t], 1)
-
-    batch_loss = (loss / int(targ.shape[1]))
-
-    variables = encoder.trainable_variables + decoder.trainable_variables
-
-    gradients = tape.gradient(loss, variables)
-
-    optimizer.apply_gradients(zip(gradients, variables))
-
-    return batch_loss
 
 
