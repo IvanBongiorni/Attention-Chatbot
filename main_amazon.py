ceb522df092a9c1450224b8a892bf1fa4eea9359
ivan <ivanbongiorni@gmail.com>
2020-03-21 18:50:44 +0100

    main wrapper to launch training

---
+++ main_amazon.py
@@ -0,0 +1,53 @@
+"""
+Author: Ivan Bongiorni,     https://github.com/IvanBongiorni
+2020-03-21
+
+KEEPS TRAINING PIPELINE TOGETHER
+
+This is supposed to be run from shell.
+"""
+
+
+def start(verbose = True):
+    import os
+    import yaml
+    import time
+    import numpy as np
+    import pandas as pd
+    import tensorflow as tf
+    
+    # Local modules
+    from dataprep import tools_amazon
+    from ml import model, train
+    
+    # Get current path
+    current_path = os.getcwd()
+    
+    # Import params and update path
+    params = yaml.load(current_path + '/config.yaml')
+    params['data_path'] = current_path + '/data/'
+    params['save_path'] = current_path + '/saved_models/'
+    
+    # Load data
+    Q_train, A_train, Q_val, A_val, Q_test, A_test, char2idx = tools_amazon.get_amazon_dataset(path = params['data_path'])
+    params['dict_size'] = len(char2idx)  # add as hyperparams to build model
+    
+    # Instantiate model
+    if params['load_saved_model']:
+        ### TODO: RESTORE SAVED MODEL
+    else:
+        model = model.build(params)
+        print('Model implemented:\n')
+        model.summary()
+    
+    # Train model
+    print('\nStart model training for {} epochs'.format(params['n_epochs']))
+    train.start(model, params, Q_train, A_train, Q_val, A_val)
+    
+    return None
+
+
+
+
+if __name__ == '__main__':
+    start()
