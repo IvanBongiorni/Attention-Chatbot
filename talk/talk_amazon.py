dd6d18cd35f4933cc1c6a9e2ba0cc902582c0939
ivan <ivanbongiorni@gmail.com>
2020-03-31 23:26:52 +0200

    prototyped talk function with recursion

--- talk/talk_amazon.py
+++ talk/talk_amazon.py
@@ -1,24 +1,61 @@
 """
 This script runs a trained Chatbot through terminal interface.
+
+It calls a talk() function that implements a Q/A exchange between user and @amazonhelp bot.
+THe model called is the one specified in params['model_name']. talk() calles itself
+recursively if the chat is not shut.
+
+Functions process_question() and process_answer() are faster copies of preprocessing
+functions from tools_amazon.py.
 """
-import tensorflow as tf
+
+### PREPROCESS input Tweet
+def process_question(tweet):
+    return tweet
 
 
+### PROCESS output Tweet
+def process_answer(tweet):
+    return tweet
 
 
+def talk(params):
+    ''' Starts chat in Terminal '''
+    import time
+    import numpy as np
+    import tensorflow as tf
 
+    print('\nInteraction with {} for @amazonhelp.'.format(params['model_name']))
+    print("\t[Type 'Quit' to exit chat]")
 
+    ### TODO: Load model
 
+    tweet = input('@User:\t')
 
+    if tweet == 'Quit':
+        print('\nShutting Chatbot down.')
+        print('Conversation closed.')
+    else:
+        tweet = process_question(tweet)
 
+        ### TODO: use time.sleep() to delay answer slightly
 
+        # Run model prediction,
+        answer = model.predict(tweet)
+        answer = process_answer(tweet)
+        print('@amazonhelp:\t{}'.format(answer))
+
+        # Recall Chatbot again
+        talk(params)
+
+    return None
 
 
 
 if __name__ == '__main__':
-    path_to_trained_model = None
-    if path_to_trained_model:
-        talk(path_to_trained_model)
-    else:
-        ## TODO: go get params dict
-        talk(params['save_model_path'])
+
+    # Load config params
+    import os
+    import yaml
+
+    talk(params)
