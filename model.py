49571cdf340b1bcb7b0f1328a9b7573c7a762801
IvanBongiorni <ivanbongiorni@gmail.com>
2020-10-24 13:37:05 +0200

    attention model and params rewritten completely

--- model.py
+++ model.py
@@ -14,103 +14,51 @@ import time
 import numpy as np
 import tensorflow as tf
 
+# Local imports
 import tools.tools_amazon as tools
-
-tools.set_gpu_configurations()
 
 
 def build(params):
-    """
-    Implements a seq2seq RNN with Convolutional self attention. It keeps a canonical
-    Encoder-Decoder structure: an Embedding layers receives the sequence of chars and
-    learns a representation. This series is received by two different layers at the same time.
-    First, an LSTM Encoder layer, whose output is repeated and sent to the Decoder. Second, a
-    block of 1D Conv layers. Their kernel filters work as multi-head self attention layers.
-    All their scores are pushed through a TanH gate that scales each score in the [-1,1] range.
-    Both LSTM and Conv outputs are concatenated and sent to an LSTM Decoder, that processes
-    the signal and sents it to Dense layers, performing the prediction for each step of the
-    output series.
-
-    Args: params dict
-    """
+    '''
+    Seq2seq RNN with Luong's multiplicative attention mechanism.
+    '''
+    import numpy as np
     import tensorflow as tf
     from tensorflow.keras.models import Model
-    from tensorflow.keras.layers import (
-        Input, Embedding, LSTM, RepeatVector, Conv1D, BatchNormalization,
-        Concatenate, LSTM, TimeDistributed, Dense
-    )
+    from tensorflow.keras.layers import Input, Embedding, LSTM, Attention, Concatenate, Dense
 
-    # ENCODER
-    encoder_input = Input(shape = (params['len_input'],))
-    encoder_embedding = Embedding(input_dim = params['dict_size'], output_dim = params['embedding_size'])(encoder_input)
+    # Encoder receives tokenized input, generates representation and gives it to LSTM
+    encoder_input = Input(shape=(None,))
+    encoder_embedding = Embedding(input_dim=params['vocab_size'], output_dim=params['embedding_size'])(encoder_input)
+    encoder_lstm = LSTM(params['encoder_lstm_units'])(encoder_embedding)
 
-    encoder_lstm = LSTM(params['len_input'], name = 'encoder_lstm')(encoder_embedding)
-    encoder_output = RepeatVector(params['len_input'], name = 'encoder_output')(encoder_lstm)
+    # Decoder receives answer through teacher forcing, then generates representation
+    # the LSTM layer produces a representation for the next char
+    decoder_input = Input(shape=(None,))
+    decoder_embedding = Embedding(input_dim=params['vocab_size'], output_dim=params['embedding_size'])(decoder_input)
+    decoder_lstm = LSTM(params['decoder_lstm_units'])(decoder_embedding)
 
-    # Convolutional block
-    conv_1 = Conv1D(
-        filters = params['conv_filters'],
-        kernel_size = params['kernel_size'],
-        activation = params['conv_activation'],
-        padding = 'same',
-        name = 'conv1')(encoder_embedding)
-    if params['use_batchnorm']:
-        conv_1 = BatchNormalization(name = 'batchnorm_1')(conv_1)
+    # Multiplicative Decoder pays attention to Encoder to produce next char prediction
+    attention = Attention()([decoder_lstm, encoder_lstm])
+    context = Concatenate()([decoder_lstm, attention])
+    decoder_output = Dense(params['vocab_size'], activation='softmax')(context)
 
-    conv_2 = Conv1D(
-        filters = params['conv_filters'],
-        kernel_size = params['kernel_size'],
-        activation = params['conv_activation'],
-        padding = 'same',
-        name = 'conv2')(conv_1)
-    if params['use_batchnorm']:
-        conv_2 = BatchNormalization(name = 'batchnorm_1')(conv_2)
-
-    conv_3 = Conv1D(
-        filters = params['conv_filters'],
-        kernel_size = params['kernel_size'],
-        activation = params['conv_activation'],
-        padding = 'same',
-        name = 'conv3')(conv_2)
-    if params['use_batchnorm']:
-        conv_3 = BatchNormalization(name = 'batchnorm_1')(conv_1)
-
-    conv_4 = Conv1D(
-        filters = params['conv_filters'],
-        kernel_size = params['kernel_size'],
-        activation = params['conv_activation'],
-        padding = 'same',
-        name = 'conv4')(conv_3)
-    if params['use_batchnorm']:
-        conv_4 = BatchNormalization(name = 'batchnorm_1')(conv_4)
-
-    # DECODER
-    concatenation = Concatenate(axis=-1, name = 'concatenation')([encoder_output, conv_4])
-
-    decoder_lstm = LSTM(params['len_input'], return_sequences = True, name = 'decoder_lstm')(concatenation)
-    decoder_dense = TimeDistributed(
-        Dense(params['decoder_dense_units'],
-              activation = params['decoder_dense_activation'],
-              name = 'decoder_dense'))(decoder_lstm)
-    decoder_output = TimeDistributed(
-        Dense(params['dict_size'],
-              activation = None,
-              name = 'decoder_output'))(decoder_dense)
-
-    model = Model(inputs = [encoder_input], outputs = [decoder_output])
+    model = Model(inputs=[encoder_input, decoder_input], outputs=[decoder_output, attention])
     return model
 
 
-def check_performance_on_test_set(model_path, X_test, Y_test):
-    import numpy as np
-    import tensorflow as tf
+### TODO: MUST BE REWRITTEN COMPLETELY
 
-    model = tf.keras.models.load_model(model_path)
-
-    # P_test = model.predict(X_test)
-    test_loss = tf.reduce_mean(
-        tf.keras.losses.sparse_categorical_crossentropy(Y_test, model(X_test),
-                                                        from_logits = True))
-
-    print("\nTest Loss: {}".format(test_loss.numpy()))
-    return None
+# def check_performance_on_test_set(model_path, X_test, Y_test):
+#     import numpy as np
+#     import tensorflow as tf
+#
+#     model = tf.keras.models.load_model(model_path)
+#
+#     # P_test = model.predict(X_test)
+#     test_loss = tf.reduce_mean(
+#         tf.keras.losses.sparse_categorical_crossentropy(Y_test, model(X_test),
+#                                                         from_logits = True))
+#
+#     print("\nTest Loss: {}".format(test_loss.numpy()))
+#     return None
